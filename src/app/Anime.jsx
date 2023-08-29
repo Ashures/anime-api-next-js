de325898af7af89e91ff59bc3d047113a325eb77
wilsjw <wilsjw2@student.op.ac.nz>
2023-08-30 09:26:56 +1200

    Added site responsiveness for smaller displays

--- src/app/Anime.jsx
+++ src/app/Anime.jsx
@@ -3,12 +3,14 @@
 export default function Anime({ anime }) {
     return (
         <div className="anime" id={anime.id}>
-            <img src={anime.image} alt={anime.title} />
+            <a href={anime.url}>
+                <img src={anime.image} alt={anime.title} />
+            </a>
             <div className="anime-info">
                 <h1>{anime.title}</h1>
                 <p>{anime.year}</p>
                 <p>episodes: {anime.episodes}</p>
-                <p>score: {anime.episodes}</p>
+                <p>score: {anime.score}</p>
             </div>
         </div>
     );
--- src/app/AnimeSearch.jsx
+++ src/app/AnimeSearch.jsx
@@ -6,7 +6,7 @@ import Anime from "./Anime";
 export default function AnimeSearch() {
     const [search, setSearch] = useState("");
     const [newSearch, setNewSearch] = useState("");
-    const [url, setUrl] = useState("https://api.jikan.moe/v4/anime?sfw&limit=24");
+    const [url, setUrl] = useState("https://api.jikan.moe/v4/top/anime?sfw&limit=24");
     const [anime, setAnime] = useState([]);
 
     useEffect(() => {
@@ -31,7 +31,7 @@ export default function AnimeSearch() {
     }, [url]);
 
     useEffect(() => {
-        setUrl(`https://api.jikan.moe/v4/anime?sfw&limit=24&q=${search}`);
+        if (search.trim() !== "") setUrl(`https://api.jikan.moe/v4/anime?sfw&limit=24&q=${search}`);
     }, [search]);
 
     function handleSubmit(e) {
@@ -49,7 +49,9 @@ export default function AnimeSearch() {
                 <label htmlFor="search">find anime</label>
                 <div className="search">
                     <input value={newSearch} onChange={e => setNewSearch(e.target.value)} type="text" id="search" />
-                    <button id="btn">search</button>
+                    <button id="btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+                    </button>
                 </div>
             </form>
             <div className="anime-display">
